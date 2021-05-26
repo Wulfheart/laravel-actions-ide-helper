58611e2a1a43fd75a597a2e7208c3a9ccf93f56a
Wulfheart <dev@alexfwulf.de>
2021-05-26 23:48:17 +0200

    Traithelpers

--- src/Service/BuildIdeHelper.php
+++ src/Service/BuildIdeHelper.php
@@ -4,12 +4,17 @@
 namespace Wulfheart\LaravelActionsIdeHelper\Service;
 
 use JetBrains\PhpStorm\Pure;
+use Lorisleiva\Actions\Concerns\AsController;
 use phpDocumentor\Reflection\DocBlock;
 use phpDocumentor\Reflection\DocBlock\Serializer;
 use phpDocumentor\Reflection\DocBlock\Tag;
 use phpDocumentor\Reflection\DocBlockFactory;
+use PhpParser\Builder\Method;
+use PhpParser\Builder\Trait_;
 use PhpParser\BuilderFactory;
 use PhpParser\Comment\Doc;
+use PhpParser\Node\Param;
+use PhpParser\Node\Stmt\Expression;
 use PhpParser\PrettyPrinter\Standard;
 
 class BuildIdeHelper
@@ -46,7 +51,7 @@ class BuildIdeHelper
             }
            $nodes[] = $ns->getNode();
         }
-
+        $nodes[] = $this->getTraitIdeHelpers($factory);
         $printer = new Standard();
         $data = $printer->prettyPrintFile($nodes);
         return $data;
@@ -64,6 +69,20 @@ class BuildIdeHelper
         $serializer = new Serializer();
 
         return $serializer->getDocComment($db);
+    }
+
+    protected function getTraitIdeHelpers(BuilderFactory $factory): \PhpParser\Node{
+        return $factory->namespace("Lorisleiva\Actions\Concerns")
+            ->addStmt(
+                (new Trait_("AsController"))->addStmt((new Method("asController"))->makePublic())
+            )->addStmt(
+                (new Trait_("AsListener"))->addStmt((new Method("asListener"))->setReturnType("void")->makePublic())
+            )->addStmt(
+                (new Trait_("AsJob"))->addStmt((new Method("asJob"))->makePublic()->setReturnType("void"))
+            )->addStmt(
+                (new Trait_("AsCommand"))->addStmt((new Method("asCommand"))->makePublic()->setReturnType("void")->addParam(new Param(new \PhpParser\Node\Expr\Variable("command"), type: "\Illuminate\Console\Command")))
+            )
+            ->getNode();
     }
 
 }
