cda6c6e854699ecb79e66df4179021b1557103c4
Alexander Wulf <alex.f.wulf@gmail.com>
2022-02-19 20:18:42 +0100

    Updated AsControllerGenerator [do nothing]

--- src/Service/Generator/DocBlock/AsControllerGenerator.php
+++ src/Service/Generator/DocBlock/AsControllerGenerator.php
@@ -3,11 +3,12 @@
 
 namespace Wulfheart\LaravelActionsIdeHelper\Service\Generator\DocBlock;
 
+use Lorisleiva\Actions\Concerns\AsController;
 use Wulfheart\LaravelActionsIdeHelper\Service\ActionInfo;
 
 class AsControllerGenerator extends DocBlockGeneratorBase implements DocBlockGeneratorInterface
 {
-    protected string $context = ActionInfo::AS_CONTROLLER_NAME;
+    protected string $context = AsController::class;
     /**
      * @inheritDoc
      */
