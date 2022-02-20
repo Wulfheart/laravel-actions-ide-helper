24684db41c4d36c5cd43daa0f6a99d3c935231c9
Alexander Wulf <alex.f.wulf@gmail.com>
2022-02-20 14:08:13 +0100

    Still failing

---
+++ vendor/Expectation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pest;
+
+/**
+ * @method mixed toBeOne()
+ * @method mixed toMatchSnapshot()
+ * @method mixed toMatchFileHashSnapshot()
+ * @method mixed toMatchFileSnapshot()
+ * @method mixed toMatchHtmlSnapshot()
+ * @method mixed toMatchJsonSnapshot()
+ * @method mixed toMatchObjectSnapshot()
+ * @method mixed toMatchTextSnapshot()
+ * @method mixed toMatchXmlSnapshot()
+ * @method mixed toMatchYamlSnapshot()
+ */
+final class Expectation {}
