d2723bfdbe7c62b34a7b09d852a54c5a74e49994
Alexander Wulf <alex.f.wulf@gmail.com>
2022-02-20 14:08:00 +0100

    Still failing

--- tests/IntersectionTypes/IntersectionAction.php
+++ tests/IntersectionTypes/IntersectionAction.php
@@ -3,12 +3,13 @@
 namespace Wulfheart\LaravelActionsIdeHelper\Tests\IntersectionTypes;
 
 use Lorisleiva\Actions\Concerns\AsObject;
+use PhpParser\Node\Expr\Array_;
 
 class IntersectionAction
 {
     use AsObject;
 
-    public function handle(\Permissionable&\Model $permissionable): ?\Permission{
+    public function handle(\stdClass&Array_ $permissionable): ?\stdClass{
         return null;
     }
 
